1e08567d150adf82d9e4636c655d32a2469b23cd
LuckyMyLove <jagiellojedrzej@gmail.com>
2021-11-04 19:22:35 +0100

    Added working fictional supply and demand

--- src/script.js
+++ src/script.js
@@ -8,8 +8,11 @@ let transportCost = []; //Macierz kosztów transportu
 let unitProfit = [[], []]; //Macierz zysków jednostkowych
 let sortedUnitProfit = []; //Posortowana tablica zyskow jednostowych
 
-let baseTransport = [[], []]; //Tabela trasy bazowej
+let baseTransport = [[], [], []]; //Tabela trasy bazowej
 
+let cumulateSupply = 0;
+let cumulateDemand = 0;
+let demandMet = 0;
 
 function getData() {
     supplierSupply = [$('#supply_D1').val(), $('#supply_D2').val()];
@@ -17,6 +20,16 @@ function getData() {
 
     customerDemand = [$('#demand_O1').val(), $('#demand_O2').val(), $('#demand_O3').val(), $('#demand_O4').val()];
     customerSelling_price = [$('#selling_price_O1').val(), $('#selling_price_O2').val(), $('#selling_price_O3').val(), $('#selling_price_O4').val()];
+
+    cumulateSupply = supplierSupply.reduce((a, b) => parseInt(a) + parseInt(b), 0);
+    cumulateDemand = customerDemand.reduce((a, b) => parseInt(a) + parseInt(b), 0);
+
+    cumulateSupply != cumulateDemand? demandMet = 1 : demandMet = 0; // 1 -> niezbilansowane, 0 -> zbilansowane
+
+    if(demandMet == 1){
+        supplierSupply.push(cumulateDemand);
+        customerDemand.push(cumulateSupply);
+    }
 
     let temp = [];
     $('.shipping_cost').each(function () {
@@ -30,19 +43,34 @@ function getData() {
 }
 
 function countUnitProfit() { //Obliczanie zysku jednostkowego (1 wynik)
-    for (let i = 0; i < 2; i++) {
-        for (let l = 0; l < 4; l++) {
-            unitProfit[i][l] = customerSelling_price[l] - transportCost[i][l] - supplierPurchase_price[i]; //Macierz zysków jednostkowych
+    for (let i = 0; i < 2+demandMet; i++) {
+        if(i == 2) unitProfit[i] = [];
+        for (let l = 0; l < 4+demandMet; l++) {
+            if(i == 2 || l == 4) unitProfit[i][l] = 0;
+            else unitProfit[i][l] = customerSelling_price[l] - transportCost[i][l] - supplierPurchase_price[i]; //Macierz zysków jednostkowych
             $('#result1_table_' + (i + 1) + '_' + (l + 1)).text(unitProfit[i][l]); //Wypisywanie danych do html
         }
     }
+    //console.log(unitProfit);
     $('#result1_header, #result1_table').show(); //Pokazywanie dotychczas ukrytej 1 tabeli z wynikami
 }
 
 function sortUnitProfit() { // konwertowanie tabeli zysk -> zysk, wiersz, kolumna wraz z sortowaniem malejaco od zysku
     let counter = 0;
-    for (let i = 0; i < 2; i++) {
-        for (let l = 0; l < 4; l++) {
+    let counterDemandMet = 0;
+    let temp = [];
+
+    for (let i = 0; i < 2+demandMet; i++) {
+        for (let l = 0; l < 4+demandMet; l++) {
+            if(i < 2 && l == 4 || i ==2){
+                temp[counterDemandMet] = [];
+                temp[counterDemandMet][0] = 0; //zysk jednostkowy w pojedynczej komórce
+                temp[counterDemandMet][1] = i; //wiersz (dostawca)
+                temp[counterDemandMet][2] = l; //kolumna (odbiorca)
+                counterDemandMet++;
+                continue
+            }
+
             sortedUnitProfit[counter] = [];
             sortedUnitProfit[counter][0] = unitProfit[i][l]; //zysk jednostkowy w pojedynczej komórce
             sortedUnitProfit[counter][1] = i; //wiersz (dostawca)
@@ -55,7 +83,7 @@ function sortUnitProfit() { // konwertowanie tabeli zysk -> zysk, wiersz, kolumn
         return a[0] - b[0];
     }).reverse();
     sortedUnitProfit.map(Number);
-    console.log(sortedUnitProfit)
+    sortedUnitProfit = sortedUnitProfit.concat(temp);
 }
 
 function calculateBaseTransportTable() { //obliczenie tabeli transportow bazowych
@@ -65,15 +93,15 @@ function calculateBaseTransportTable() { //obliczenie tabeli transportow bazowyc
 
     let customersWithTransport = []; //tablica na obsluzonych odbiorcow (bez popytu)
     let suppliersWithTransport = []; //tablica na obsluzonych dostawcow (bez podazy)
-
-    for (let i = 0; i < 8; i++) { //iterujemy po posortowanej tabeli tras najbardziej zyskownych
+    console.log(baseTransport)
+    for (let i = 0; i < sortedUnitProfit.length; i++) { //iterujemy po posortowanej tabeli tras najbardziej zyskownych
 
         //mozna by to nazwa row i col, nie byloby zdziwienia ze jest na odwrot przy uzyciu
 
         let col = sortedUnitProfit[i][1];  //wybor dostawcy
         let row = sortedUnitProfit[i][2];  //wybor odbiorcy
 
-        //console.log(y + ", " + x + " - " + supplierSupply[y] + ", " + customerDemand[x]);
+        //console.log('row: ' + col + ", kolumna: " + row + ", " + supplierSupply[col] + '(podaż) - ' + customerDemand[row] + "(popyt), ");
 
         if (customersWithTransport.includes(row) || suppliersWithTransport.includes(col)) {
             //jesli w danej komorce nie ma popytu lub podazy, skipujemy
@@ -81,11 +109,11 @@ function calculateBaseTransportTable() { //obliczenie tabeli transportow bazowyc
             continue;
         }
 
+        //sytuacja gdy mozemy spelnic w calosci zapotrzebowanie
         if (supplierSupply[col] >= customerDemand[row]) {
-            //sytuacja gdy mozemy spelnic w calosci zapotrzebowanie
             //console.log("Zapasy wieksze lub rowne niz zapotrzebowanie")
             baseTransport[col][row] = customerDemand[row]; //w to miejsce wieziemy tyle ile popyt
-            supplierSupply[col] = supplierSupply[col] - customerDemand[row]; //zmniejszamy ilosc na stanie (podaz)
+            supplierSupply[col] = parseInt(supplierSupply[col]) - parseInt(customerDemand[row]); //zmniejszamy ilosc na stanie (podaz)
             customerDemand[row] = 0; //zaspokojony popyt, nie wiem czy potrzebne?
             customersWithTransport.push(row) //ten klient juz nie potrzebuje ("iksy" w tej kolumnie)
             if (supplierSupply[col] == 0) //jesli przy tej akcji skonczyly sie zapasy, to dostawca tez na liste
@@ -94,26 +122,33 @@ function calculateBaseTransportTable() { //obliczenie tabeli transportow bazowyc
             //nie mozemy sprostac calemu zapotrzebowaniu
             //console.log("Zapasy mniejsze niz zapotrzebowanie")
             baseTransport[col][row] = supplierSupply[col]; //dajemy tyle ile ma dostwca
-            customerDemand[row] = customerDemand[row] - supplierSupply[col]; //tutaj zeby sie nie zminusowalo
+            customerDemand[row] = parseInt(customerDemand[row]) - parseInt(supplierSupply[col]); //tutaj zeby sie nie zminusowalo
+            //$('.fictional').show(); //wyswietlanie fikcyjnego dostawcy i odbiorcy
+
+
+
             supplierSupply[col] = 0; //nie wiem czy potrzebne
             suppliersWithTransport.push(col);
             if (customersWithTransport[row] == 0) //to tez nie wiem czy potrzebne
                 customersWithTransport.push(row);
         }
+
     }
 
-
-    for (let i = 0; i < 2; i++) { //wincyj pentli, WINCYJ
-        for (let l = 0; l < 4; l++) {
+    for (let i = 0; i < 2+demandMet; i++) { //wincyj pentli, WINCYJ
+        for (let l = 0; l < 4+demandMet; l++) {
             $('#result2_table_' + (i + 1) + '_' + (l + 1)).text(baseTransport[i][l]);//podmiana wartosci w result2
         }
     }
+    if(demandMet == 1) $('.fictional').show();
+    console.log(baseTransport)
     $('#result2_header, #result2_table').show(); //wyswietlanie
 }
 
 $(document).ready(function () { //Główna funkcja, tutaj piszemy kod
     $('#count').click(function () {
         if ($("#main_form")[0].checkValidity()) { //Jeśli formularz jest w pełni wypełniony
+            $('#result1_header, #result1_table, #result2_header, #result2_table, .fictional').hide(); //wyswietlanie
             getData(); //Pozyskiwanie danych do zmiennych
             countUnitProfit(); //Wynik 1
             sortUnitProfit(); //Sortowanie od tras najbardziej zyskownych
